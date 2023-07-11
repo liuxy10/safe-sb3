36eacd497f6ad63882503d15290f5e3e9883d56c
liuxy10 <liuxytxdy10@gmail.com>
2023-07-11 13:29:29 -0700

    save h5py and model for visualization

--- examples/metadrive/link_data_folder.sh
+++ examples/metadrive/link_data_folder.sh
@@ -1,2 +1,7 @@
+# examples
+# link checkpoints 
 ln -s /home/vision/src/data/metadrive/saved_bc_policy /home/vision/src/xinyi/safe-sb3/examples/metadrive/saved_policy
-
+# copy h5py to github for visualization on pc
+cp  /home/vision/src/data/metadrive/h5py/bc_9_900.h5py /home/vision/src/xinyi/safe-sb3/examples/metadrive/h5py/bc_9_900.h5py
+# copy checkpoint model to github for visualization on 
+cp  /home/vision/src/data/metadrive/saved_bc_policy/bc-waymo-es0_120000_steps.zip /home/vision/src/xinyi/safe-sb3/examples/metadrive/saved_bc_policy/bc-waymo-es0.zip
