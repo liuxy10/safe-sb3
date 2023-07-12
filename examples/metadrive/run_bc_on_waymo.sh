137b8b76e1221bc8806572d9cd3763a9ac5d7e0b
liuxy10 <liuxytxdy10@gmail.com>
2023-07-12 00:33:31 -0700

    add sac baseline experiment

--- examples/metadrive/run_bc_on_waymo.sh
+++ examples/metadrive/run_bc_on_waymo.sh
@@ -1,12 +1,12 @@
 # collect pkl env data from waymo raw data
-python examples/metadrive/waymo_utils.py --tfrecord_dir examples/metadrive/tfrecord_9 --pkl_dir examples/metadrive/pkl_9
+# python examples/metadrive/waymo_utils.py --tfrecord_dir examples/metadrive/tfrecord_9 --pkl_dir examples/metadrive/pkl_9
 # alternatively on server:
 # python examples/metadrive/waymo_utils.py --tfrecord_dir ~/src/data/metadrive/tfrecord_9/  --pkl_dir ~/src/data/metadrive/pkl_9/
 
 # run pkl files in waymo env to collect h5py RL data for offline RL training
-python examples/metadrive/collect_h5py_from_pkl.py --pkl_dir examples/metadrive/pkl_9 --h5py_path examples/metadrive/h5py/one_pack_training.h5py
+# python examples/metadrive/collect_h5py_from_pkl.py --pkl_dir examples/metadrive/pkl_9 --h5py_path examples/metadrive/h5py/one_pack_training.h5py
 # alternatively on server:
-# python examples/metadrive/collect_h5py_from_pkl.py --pkl_dir ~/src/data/metadrive/pkl_9/ --h5py_path ~/src/data/metadrive/h5py/bc_9_900.h5py --num_of_scenarios 900 
+python examples/metadrive/collect_h5py_from_pkl.py --pkl_dir ~/src/data/metadrive/pkl_9/ --h5py_path ~/src/data/metadrive/h5py/bc_9_900.h5py --num_of_scenarios 900 
 
 
 # train offline BC policy 
@@ -15,5 +15,5 @@ python examples/metadrive/collect_h5py_from_pkl.py --pkl_dir examples/metadrive/
 
 # alternatively on server:
 python examples/metadrive/run_bc_waymo.py --pkl_dir ~/src/data/metadrive/pkl_9/ --h5py_path ~/src/data/metadrive/h5py/bc_9_900.h5py --output_dir ~/src/data/metadrive/saved_bc_policy --num_of_scenarios 900 --steps 1000000 --save_freq 10000
-
+# python examples/metadrive/run_sac_waymo.py --pkl_dir ~/src/data/metadrive/pkl_9/ --output_dir ~/src/data/metadrive/saved_sac_policy --num_of_scenarios 900 --steps 1000000 --save_freq 10000
 
