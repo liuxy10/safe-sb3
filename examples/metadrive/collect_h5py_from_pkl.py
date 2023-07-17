bug fixed: angle wrap added for heading rate calculation

examples/metadrive/collect_h5py_from_pkl.py
@@ -4,7 +4,7 @@ import numpy as np
 from metadrive.policy.replay_policy import ReplayEgoCarPolicy
 from metadrive.policy.env_input_policy import EnvInputHeadingAccPolicy
 # from metadrive.utils.coordinates_shift import waymo_2_metadrive_heading, waymo_2_metadrive_position
-from utils import get_acc_from_vel, get_local_from_heading, get_acc_from_speed
+from utils import get_acc_from_vel, get_local_from_heading, get_acc_from_speed, get_rate_from_heading
 
 import tqdm
 import h5py
@@ -46,7 +46,7 @@ def get_current_ego_trajectory_old(waymo_env,i):
     # local_vel = get_local_from_heading(velocity, heading) # not used
     # local_acc = get_acc_from_vel(local_vel, ts)
     acc = get_acc_from_speed(speed, ts)
-    heading_speed = get_acc_from_speed(heading, ts)
+    heading_speed = get_rate_from_heading(heading, ts)
 
     print("max speed, avg speed, acc range,  heading range = {:.{}f}, {:.{}f}, [{:.{}f}, {:.{}f}], [{:.{}f}, {:.{}f}]".format(
            np.max(speed), 3, np.mean(speed), 3, np.min(acc), 3, np.max(acc), 3, np.min(heading), 3, np.max(heading), 3))

examples/metadrive/visualize.py
@@ -68,7 +68,7 @@ def visualize_h5py(args):
                 data_name = dataset_names[i]
                 data = np.array(hf[data_name])
                 # if data_name == 'heading_rates':
-                #     # print(data)
+                    # print(data, np.mean(data), np.max(data), np.min(data))
                 ax.hist(data, bins='auto')
                 ax.set_title(data_name)
                 ax.set_xlabel('Value')
@@ -114,8 +114,8 @@ def visualize_bc_prediction(args):
     env.seed(args["env_seed"])
     
     
-    # model = BC.load(args['model_path'])
-    model = SAC.load(args['model_path'])
+    model = BC.load(args['model_path'])
+    # model = SAC.load(args['model_path'])
     for seed in range(0, num_scenarios):
             o = env.reset(force_seed=seed)
             #ts, position, velocity, acc, heading
@@ -190,9 +190,9 @@ def visualize_bc_prediction(args):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--h5py_path', type=str, default='examples/metadrive/bc_9_900.h5py')
+    parser.add_argument('--h5py_path', type=str, default='/home/xinyi/Documents/UCB/safe-sb3/examples/metadrive/h5py/pkl9_900.h5py')
     parser.add_argument('--pkl_dir', '-pkl', type=str, default='examples/metadrive/pkl_9')
-    parser.add_argument('--model_path', '-out', type=str, default='examples/metadrive/example_policy/sac-waymo-es0_770000_steps-heading.zip')
+    parser.add_argument('--model_path', '-out', type=str, default='examples/metadrive/example_policy/bc-waymo-es0.zip')
     parser.add_argument('--env_seed', '-es', type=int, default=0)
     parser.add_argument('--lambda', '-lam', type=float, default=1.)
     parser.add_argument('--num_of_scenarios', type=str, default="100")